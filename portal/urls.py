c6d7e756d7bfd489805a602f0e9c90edbcbbedb0
wlkraus1 <Tyler.Krause@icloud.com>
2025-08-10 22:57:13 -0400

    Update urls.py

--- portal/urls.py
+++ portal/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views
-
 urlpatterns = [
     path("", views.home, name="portal_home"),
     path("household/<int:pk>/", views.household_view, name="portal_household"),
